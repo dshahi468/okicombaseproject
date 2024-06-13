785e1d2081309b7b28ac41bbfd52924ccbb3959f
Dil Shahi <dshahi468@gmail.com>
2024-06-13 18:37:46 +0900

    Frontend configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,10 +6,11 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  server: {
+    port: 3000,
+    host: '0.0.0.0'
+  },
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
